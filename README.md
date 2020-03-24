8b180b6c868c2ff112e4e07538e2557c9fc2656a
isaac4everlast <45059720+isaac4everlast@users.noreply.github.com>
2020-03-24 15:10:23 +0430

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# RENAMER
+
+Rename existing html files with the content of the <title> tag of the html file and the < meta > tag that contains the publication date.
+
+(Specially made for some radom dude on StackOverflow)
---
+++ main.js
@@ -0,0 +1,50 @@
+const fs = require("file-system");
+const path = require("path");
+const readline = require("readline");
+
+var pathToEdit = path.join(__dirname + "/../");
+
+
+fs.readdir(pathToEdit, (err, files) => {
+  if(err) throw err;
+  console.log("Renaming files, please wait...");
+    files.forEach(file => {
+
+
+ 
+        //Go through each and every html file:
+        //Check if the file is an html file
+        if(file.split(".")[1] == "html") {
+          var dateVal = null;
+          var title = null;
+          var readInterface = readline.createInterface({
+            input: fs.createReadStream(pathToEdit + "/" + file),
+            console: false
+          });
+          readInterface.on('line', function(line) {
+            if(line.split("property=").length > 1) {
+            if(line.split("property=")[1].substring(1,26) == "og:article:published_time") {
+              //Get the modified date
+              dateVal = line.split("content=")[1].substring(1,11).split("-");
+            }
+            
+          }
+          if(line.split("<title>").length > 1) {
+            title = line.split("<title>")[1].substring(0, line.split("<title>")[1].length - 8)
+          }
+
+          
+          
+        });
+        setTimeout(function() {
+        if(dateVal != null && title != null) {
+          fs.rename(pathToEdit + "/" + file, pathToEdit + "/" + title + "_" + dateVal[0] + dateVal[1] + dateVal[2] + ".html", (err) => {
+              if(err) throw err;
+            });  
+        }
+        }, 100);
+        
+      }
+    });
+    console.log("Done!");
+  });
